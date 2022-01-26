6da74d8942302593764a31db07359f6b4b931098
RaghadAbdulhadi <raghadabdulhadi1999@gmail.com>
2022-01-26 15:34:26 +0200

    more styles will be added

--- Demo/js/app.js
+++ Demo/js/app.js
@@ -1,10 +1,10 @@
 'use strict';
-function GenerateEmployee(emplyeeId, fullName, department, level) {
+function GenerateEmployee(emplyeeId, fullName, department, level, imageUrl) {
     this.emplyeeId = emplyeeId;
     this.fullName = fullName;
     this.department = department;
     this.level = level;
-    this.imageUrl = `./images/${this.fullName}.PNG`;
+    this.imageUrl = imageUrl;
 }
 
 let min = 0;
@@ -27,10 +27,9 @@ GenerateEmployee.prototype.salary = function () {
     let tax = randomSalary * 0.075;
     this.salary = randomSalary - tax;
 }
-// GenerateEmployee.prototype.emplyeeId = function() {
-//    let emplyeeRandomId =  Math.floor(Math.random() * 899999 + 100000);
-
-// }
+function emplyeeId() {
+   return  Math.floor(Math.random() * 899999 + 100000);
+}
 
 GenerateEmployee.prototype.render = function () {
     // document.write(`<p>${this.fullName}</p>`);
@@ -40,46 +39,49 @@ GenerateEmployee.prototype.render = function () {
     let newEmployeeCard = document.getElementById('addEmployeeSection');
     let employeeCard = document.createElement('div');
     newEmployeeCard.appendChild(employeeCard);
+    // employeeCard.style.backgroundColor = "red";
 
     //Add Image for each employee in img tag and append it to employeeCard
     let employeePic = document.createElement('img');
-    newEmployeeCard.appendChild(employeePic);
+    employeeCard.appendChild(employeePic);
     employeePic.setAttribute('src', this.imageUrl);
+    employeePic.style.width = '200px'
+    employeePic.style.borderRadius = "200px"
 
     //Add Emplyee Full Name
-    let employeeFullName = document.createElement('h2');
-    newEmployeeCard.appendChild(employeeFullName);
+    let employeeFullName = document.createElement('h3');
+    employeeCard.appendChild(employeeFullName);
     employeeFullName.textContent = `Employee Name: ${this.fullName}`;
 
     // //Add EmployeeId to the newEmployeeCard 
-    // let newEmployeeId = document.createElement('h3');
-    // newEmployeeCard.appendChild(newEmployeeId);
-    // newEmployeeId.textContent = 
+    let newEmployeeId = document.createElement('p');
+    employeeCard.appendChild(newEmployeeId);
+    newEmployeeId.textContent = `Employee Id: ${this.emplyeeId}`;
 
     //Add Department 
-    let emplyeeDepartment = document.createElement('h4');
-    newEmployeeCard.appendChild(emplyeeDepartment);
+    let emplyeeDepartment = document.createElement('p');
+    employeeCard.appendChild(emplyeeDepartment);
     emplyeeDepartment.textContent = `Department: ${this.department}`;
 
     //Add Level
-    let employeeLevel = document.createElement('h5');
-    newEmployeeCard.appendChild(employeeLevel);
+    let employeeLevel = document.createElement('p');
+    employeeCard.appendChild(employeeLevel);
     employeeLevel.textContent = `Level: ${this.level}`;
 
 
     //Add Salary
-    let employeeSalary = document.createElement('h6');
-    newEmployeeCard.appendChild(employeeSalary);
+    let employeeSalary = document.createElement('p');
+    employeeCard.appendChild(employeeSalary);
     employeeSalary.textContent = `Salary: ${this.salary}`;
 }
 
-let employeeOne = new GenerateEmployee(1000, 'Ghazi Samer', 'Administration', 'Senior');
-let employeeTwo = new GenerateEmployee(1001, 'Lana Ali', 'Finance', 'Senior');
-let employeeThree = new GenerateEmployee(1002, 'Tamara Ayoub', 'Marketing', 'Senior');
-let employeeFour = new GenerateEmployee(1003, 'Safi Walid', 'Administration', 'Mid-Senior');
-let employeeFive = new GenerateEmployee(1004, 'Omar Zaid', 'Development', 'Senior');
-let employeeSix = new GenerateEmployee(1005, 'Rana Saleh', 'Development', 'Junior');
-let employeeSeven = new GenerateEmployee(1006, 'Hadi Ahmad', 'Finance', 'Mid-Senior');
+let employeeOne = new GenerateEmployee(1000, 'Ghazi Samer', 'Administration', 'Senior','./images/GhaziSamer.jpg');
+let employeeTwo = new GenerateEmployee(1001, 'Lana Ali', 'Finance', 'Senior', './images/LanaAli.jpg');
+let employeeThree = new GenerateEmployee(1002, 'Tamara Ayoub', 'Marketing', 'Senior', './images/TamaraAyoub.jpg');
+let employeeFour = new GenerateEmployee(1003, 'Safi Walid', 'Administration', 'Mid-Senior', './images/SafiWalid.jpg');
+let employeeFive = new GenerateEmployee(1004, 'Omar Zaid', 'Development', 'Senior','./images/OmarZaid.jpg');
+let employeeSix = new GenerateEmployee(1005, 'Rana Saleh', 'Development', 'Junior','./images/RanaSaleh.jpg');
+let employeeSeven = new GenerateEmployee(1006, 'Hadi Ahmad', 'Finance', 'Mid-Senior','./images/HadiAhmad.jpg');
 
 let employees = [employeeOne, employeeTwo, employeeThree, employeeFour, employeeFive, employeeSix, employeeSeven];
 
@@ -101,9 +103,9 @@ function addNewEmployee(event) {
     let fullName = event.target.fullName.value;
     let department = event.target.department.value;
     let level = event.target.level.value;
-    let imageurl = event.target.imageurl.value;
-    console.log(fullName, department, level, imageurl);
-    let EmployeesNewCard = new GenerateEmployee(fullName, department, level, imageurl);
+    let imageUrl = event.target.imageUrl.value;
+    console.log(fullName, department, level, imageUrl);
+    let EmployeesNewCard = new GenerateEmployee(emplyeeId(),fullName, department, level, imageUrl);
     EmployeesNewCard.salary();
     EmployeesNewCard.render();
 }
